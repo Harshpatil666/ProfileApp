0f328026b05dd0fca9a16004b39574a3e261679a
shorifkhan55 <shorifulba@gmail.com>
2016-11-30 00:18:22 +0600

    add Full name Method to student class

--- ProfileApp/Student.cs
+++ ProfileApp/Student.cs
@@ -8,6 +8,13 @@ namespace ProfileApp
     public class Student
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public void FullName()
+        {
+            FirstName + " " + LastName;
+        }
     }
 }
